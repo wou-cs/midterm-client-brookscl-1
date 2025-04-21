710f7c34196eb9f140acaf11271cfa1adc754123
Chris Brooks <chris@chrisbrooks.org>
2025-04-21 10:15:50 -0400

    Committing for autograder

--- client.py
+++ client.py
@@ -1,27 +1,28 @@
 import requests
 
 
+base_url = "http://chrisbrooks.pythonanywhere.com/"
+
+
 def get_programmer_count():
-    """
-    Return the number of programmers return from the plural programmers API
-    :return: An integer indicating the number of programmers in the plural list.
-    """
-    return 0
+    r = requests.get(base_url + 'api/programmers')
+    programmers = r.json()
+    return len(programmers['programmers'])
 
 
 def get_programmer_by_id(pid):
-    """
-    Return the single programmer referenced by the specified programmer id (pid)
-    :param pid: Unique identifier for the programmer to lookup
-    :return: A dictionary containing the matched programmer. Return an empty dictionary if not found
-    """
-    return {}
+    r = requests.get(base_url + f"api/programmers/{pid}")
+    if r.status_code == 200:
+        return r.json()
+    else:
+        return {}
+        # return None
 
 
 def get_full_name_from_first(first_name):
-    """
-    Return the full name of the *first* programmer having the provided first name, concatenating the first and last name with a space between.
-    :param first_name:
-    :return: A string containing the first and last name of the first programmer in the list of matches.
-    """
-    return ""
+    r = requests.get(base_url + f"api/programmers/by_first_name/{first_name}")
+    programmer_list = r.json()['programmers']
+    if len(programmer_list) == 0:
+        return None
+    else:
+        return programmer_list[0]['first'] + " " + programmer_list[0]['last']
